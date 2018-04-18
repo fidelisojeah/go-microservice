4bd350adaf3a829a329ae9dd9d67f51fbf7c54c9
Fidelis Ojeah <fidelis.ojeah@gmail.com>
2018-04-18 20:40:50 +0100

    changes

--- user-service/proto/auth/extensions.go
+++ user-service/proto/auth/extensions.go
@@ -2,13 +2,15 @@ package auth
 
 import (
 	"github.com/jinzhu/gorm"
-	uuid "github.com/satori/go.uuid"
+	"github.com/satori/go.uuid"
 )
 
 // BeforeCreate - hook to create hashed id
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
 
-	newUUID := uuid.Must(uuid.NewV4())
-
+	newUUID, err := uuid.NewV4()
+	if err != nil {
+		return err
+	}
 	return scope.SetColumn("Id", newUUID.String())
 }
